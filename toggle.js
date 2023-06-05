7066af42cbd61e6ba1fd702aa6096f04c7255c53
Soraia <115974717+SoraiaBarroso@users.noreply.github.com>
2023-06-05 11:52:38 +0200

    Update toggle.js

--- toggle.js
+++ toggle.js
@@ -3,12 +3,12 @@ $(document).ready(function() {
 
     $("#myButton").click(function () {
         if (isMoved) {
-            $("#myButton").css({"left": "0", "background-image": "url(/Toggle_buttom/icons8-sun-48.png)"});
+            $("#myButton").css({"left": "0", "background-image": "url(./icons8-sun-48.png)"});
             $("body").css("background", "#a8c5f8");
             $(".container").css("box-shadow", "inset -2px 2px 5px #3e6799")
             isMoved = false;
         }else{
-            $("#myButton").css({"left": "80px", "background-image": "url(/Toggle_buttom/icons8-moon-48.png)"});
+            $("#myButton").css({"left": "80px", "background-image": "url(./icons8-moon-48.png)"});
             $("body").css("background", "#43474b");
             $(".container").css("box-shadow", "inset 2px -2px 5px #becbd9")
             isMoved = true;
